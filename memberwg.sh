cad4073d26699b41ab550ee5326252dbba2ad5f4
Potato <68516497+potatonc@users.noreply.github.com>
2021-02-20 13:25:19 +0700

    Add files via upload

---
+++ memberwg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script Wireguard by Potato
+
+clear
+echo ""
+echo "===================================================="
+echo "|             Daftar Member Wireguard              |"
+echo "===================================================="
+echo ""
+echo ""
+cat /etc/anc/memberwg/*
+echo ""
+echo ""
+echo "----------------------------------------------------"
+echo "           Terimakasih sudah menggunakan-"
+echo "              Script Credit by Potato"
+echo "----------------------------------------------------"
+echo ""
